179c9b0658a42a739e73563075b998c415fd60a7
Marko Obradović <marko.obradovic@infobip.com>
2014-11-21 09:43:26 +0100

    1.0-SNAPSHOT Initial project commit

---
+++ src/main/java/configuration/ContextConfiguration.java
@@ -0,0 +1,33 @@
+package configuration;
+
+import com.mongodb.Mongo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+import java.net.UnknownHostException;
+
+/**
+ * Created by mobradovic on 11/21/14.
+ */
+public class ContextConfiguration {
+
+    public static final String HOST = "localhost";
+    public static final int PORT = 12345;
+    public static final String DATABASE_NAME = "dbName";
+
+    @Bean
+    public MongoDbFactory getMongo() {
+        try {
+            return new SimpleMongoDbFactory(new Mongo(HOST, PORT), DATABASE_NAME);
+        } catch (UnknownHostException e) {
+            return null; //edit
+        }
+    }
+
+    @Bean
+    public MongoTemplate getMongoTemplate() {
+       return new MongoTemplate(getMongo());
+    }
+}
